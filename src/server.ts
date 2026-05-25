be8bacd2f988eddc128dfdb03666dcd9675311ba
Arindam Saha <arindam.ui@gmail.com>
2026-05-25 12:55:01 +0530

    add express setup

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,21 @@
-import {Configs} from "./config/index.js";
+import {Configs} from "./config";
+import app from "./app";
 
 console.log(Configs.PORT);
+
+
+function startServer() {
+    const PORT = Configs.PORT;
+
+    try {
+       
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error("Error starting server:", error);
+        process.exit(1);
+    }
+    // Your server code here
+}
+startServer();
