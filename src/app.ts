a844bf3858c21691f2f92483d9ef7cfb43efe227
Arindam Saha <arindam.ui@gmail.com>
2026-05-25 18:40:01 +0530

    add Errorhandeling

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,35 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import logger from './config/logger';
+import createHttpError, { HttpError } from 'http-errors';
 
 const app = express();
 
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the auth service');
+app.get('/', (req, res, next) => {
+  const err = createHttpError(401, 'Unauthorized access to the auth service');
+  next(err);
+
+  //res.send('Welcome to the auth service');
+});
+
+
+
+// global error handler. and it should be the last middleware in the chain
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err:HttpError, req:Request, res:Response, next:NextFunction) => {
+  logger.error({ message: err.message });
+
+
+  const statusCode = err.status || 500;
+  res.status(statusCode).json({
+    error: {
+      message: err.message,
+      type: err.name,
+      status: statusCode,
+      path: '',
+      location: '',
+    },
+  });
 });
 
 
---
+++ src/config/logger.ts
@@ -0,0 +1,34 @@
+import winston from "winston";
+import { Configs } from ".";
+
+const logger = winston.createLogger({
+  level: "info",
+  defaultMeta: { service: "auth-service" },
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File(
+        { 
+            dirname: "logs",
+            filename: "error.log",
+            level: "error",
+            silent: Configs.NODE_ENV === "test" // Disable file logging during tests 
+        }
+    ),
+    new winston.transports.File(
+        { 
+            dirname: "logs",
+            filename: "combined.log",
+            format: winston.format.combine( winston.format.timestamp(), winston.format.json() ),
+            level: "info", 
+            silent: Configs.NODE_ENV === "test" // Disable file logging during tests
+        }
+    ),
+    new winston.transports.Console({
+        format: winston.format.combine( winston.format.timestamp(), winston.format.json() ),
+        level: "info",
+        silent: Configs.NODE_ENV === "test" // Disable console logging during tests
+    }),
+  ],
+});
+
+export default logger;
