80f6ad7fc9c8f9c84ca0a877d7f7111719e51029
Arindam Saha <arindam.ui@gmail.com>
2026-06-05 00:46:58 +0530

    created db connection and added test

--- test/users/register.spec.ts
+++ test/users/register.spec.ts
@@ -1,9 +1,33 @@
 import request from "supertest";
-import {describe, expect, it} from '@jest/globals';
+import {beforeAll,beforeEach, afterAll,describe, expect, it} from '@jest/globals';
 import app from "../../src/app";
+import {DataSource} from "typeorm";
+import {AppDataSource} from "../../src/config/data-source";
+import {User} from "../../src/entities/User";
+import { truncateTables } from "../utils";
+
+
+
+
 describe("POST /auth/register", () => {
   
     describe("given valid user data", () => {
+
+        let connections: DataSource;
+
+        beforeAll(async () => {
+            connections = await AppDataSource.initialize();
+        });
+
+        beforeEach(async () => {
+            await truncateTables(connections);
+            //await connections.getRepository(User).clear();
+        });
+
+        afterAll(async () => {
+            await connections.destroy();
+        });
+
         it("should register the user and return a success message", async () => { 
             // Arrange
             const userData = {
@@ -52,6 +76,13 @@ describe("POST /auth/register", () => {
                 .post("/auth/register")
                 .send(userData);
             // Assert
+            const userRepository = connections.getRepository(User);
+
+            const user = await userRepository.find();
+
+            expect(user.length).toHaveLength(1);
+
+
         });
     });
 
